adc1a8706ddea4921bae86c94323c5a8626ff298
billkevinmaina <hitkenyan@gmail.com>
2025-05-02 10:11:04 +0300

    D

--- whileLoops.py
+++ whileLoops.py
@@ -48,24 +48,36 @@
 
 # print(multiply(x, y))	
 
+# def reverse_string(word):
+#     # Base case: if the word is empty or just one letter, return it
+#     if len(word) <= 1:
+#         return word
+#     else:
+#         # Take the rest of the word (except the first letter), reverse it,
+#         # and then add the first letter at the end
+#         return reverse_string(word[1:]) + word[0]
 
+# # Try it with a word
+# word = "cat"
+# reversed_word = reverse_string(word)
+# print(reversed_word)
 
 #7. reccursive function that determines whether a given number n 
 # is a prime number by checking for divisiblility by intergers less then n
-def is_prime(n, divisor=None):
-    if divisor is None:
-        divisor = n - 1
-    if n <= 1:
-        return False
-    if divisor == 1:
-        return True
-    if n % divisor == 0:
-        return False
-    return is_prime(n, divisor - 1)
+# def is_prime(n, divisor=None):
+#     if divisor is None:
+#         divisor = n - 1
+#     if n <= 1:
+#         return False
+#     if divisor == 1:
+#         return True
+#     if n % divisor == 0:
+#         return False
+#     return is_prime(n, divisor - 1)
 
-# Test
-num = int(input("Enter a number(n) to check if it's prime: "))
-if is_prime(num):
-    print(num, "is a prime number.")
-else:
-    print(num, "is not a prime number.")
+# # Test
+# num = int(input("Enter a number(n) to check if it's prime: "))
+# if is_prime(num):
+#     print(num, "is a prime number.")
+# else:
+#     print(num, "is not a prime number.")
